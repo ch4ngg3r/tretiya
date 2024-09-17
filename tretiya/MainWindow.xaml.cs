16144b1b6f999415654a85db670e2272dd9f63a0
Денис Миниязов < @42-7>
2024-09-17 07:59:23 +0300

    Добавьте файлы проекта.

---
+++ tretiya/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tretiya
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static List<UserScopedSettingAttribute> users = new List<user>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        void Load()
+        {
+            users.Add(new user)
+                {
+                name = name.Text,
+                password = password.Text,
+                };
+
+        }
+        public class user
+        {
+            public string name(get; set;)
+                public string password (get; set;)
+        }
+
+    }
+}
